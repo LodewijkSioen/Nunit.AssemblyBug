c4320b379f11271017d5b80374406122ad6fdff1
Lodewijk Sioen <lodewijk.sioen@codit.eu>
2026-04-06 17:18:50 +0200

    Added Extra test with interesting outcome

--- Reproduction.cs
+++ Reproduction.cs
@@ -39,21 +39,33 @@ public class Reproduction
         Assert.That(result, Is.EqualTo($"\"{Holder.TestValue.ToString()}\""));
     }
 
+    [Test]
+    public async Task TestWolverineViaBus()
+    {
+        var response = await _host.Scenario(s =>
+        {
+            s.Get.Url("/wolverine-bus");
+            s.StatusCodeShouldBe(200);
+        });
+        var result = await response.ReadAsTextAsync();
+
+        using var scope = Assert.EnterMultipleScope();
+        Assert.That(result, Is.EqualTo($"\"{Holder.TestValue.ToString()}\""));
+    }
+
     [SetUp]
     public async Task Setup()
     {
         var builder = WebApplication.CreateBuilder();
-        builder.Host.UseWolverine(opts =>
-        {
-            opts.Discovery.DisableConventionalDiscovery();
-            opts.Discovery.IncludeAssembly(GetType().Assembly);
-        });
+        builder.Host.UseWolverine();
         builder.Services.AddWolverineHttp();
 
         _host = await AlbaHost.For(builder, app =>
         {
             app.MapWolverineEndpoints();
             app.MapGet("/minimal", () => Results.Ok(Holder.TestValue));
+            app.MapGet("wolverine-bus",
+                (IMessageBus bus) => bus.InvokeAsync<Guid>(new WolverineClassicHandler.Request()));
         });
     }
 
@@ -72,10 +84,19 @@ public static class Holder
 
 public class WolverineHttpEndpoint
 {
-
     [WolverineGet("/wolverine")]
     public IResult Handle()
     {
         return Results.Ok(Holder.TestValue);
+    }
+}
+
+public class WolverineClassicHandler
+{
+    public record Request;
+
+    public Guid Handle(Request request)
+    {
+        return Holder.TestValue;
     }
 }
